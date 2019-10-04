72065185d1f669e4074b02a6cc0b0fd8fa63c2b4
alineah <support@scalewatch.com>
2019-10-04 23:15:06 +0200

    Here we manage the different Simplx versions.

---
+++ include/simplx_core/engineversion.h
@@ -0,0 +1,12 @@
+/**
+ * @file engineversion.h
+ * @brief Simplx version information
+ * @copyright 2019 Scalewatch (www.scalewatch.com). All rights reserved.
+ * Please see accompanying LICENSE file for licensing terms.
+ */
+
+#pragma once
+
+#define SIMPLX_ENGINE_VERSION_MAJOR 3                     // major version
+#define SIMPLX_ENGINE_VERSION_MINOR 0                     // minor version
+#define SIMPLX_ENGINE_VERSION_PATCH 0                    // patch version
