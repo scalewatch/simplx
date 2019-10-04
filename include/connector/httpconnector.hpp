b59ada873ba01ff9ee202c730b67d2c80c3ab2b9
vvalerian <valerian.vives@gmail.com>
2019-10-05 00:08:52 +0200

    Top level header to include http headers & dependencies

---
+++ include/connector/httpconnector.hpp
@@ -0,0 +1,16 @@
+/**
+ * @author Valerian Vives <valerian.vives@scalewatch.com>
+ * @file httpconnector.hpp
+ * @brief http connector top-level header
+ * @copyright 2019 Scalewatch (www.scalewatch.com). All rights reserved.
+ * Please see accompanying LICENSE file for licensing terms.
+ */
+
+#pragma once
+
+#include "connector/http/server/server.hpp"
+#include "connector/http/server/serverprocess.hpp"
+
+
+using simplx::connector::http::HttpServer;
+using simplx::connector::http::HttpServerProcess;
