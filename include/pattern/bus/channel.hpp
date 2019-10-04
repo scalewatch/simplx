918892c11b595dc2402dab75d8f6d0deea99921a
vvalerian <valerian.vives@gmail.com>
2019-10-05 00:25:37 +0200

    class handling publisher, subscriber, buffer for a chanel (thread to thread directional communication)

--- include/pattern/bus/channel.hpp
+++ include/pattern/bus/channel.hpp
@@ -1,3 +1,10 @@
+/**
+ * @file channel.hpp
+ * @brief class handling publisher, subscriber, buffer 
+for a chanel (thread to thread directional communication)
+ * @copyright 2019 Scalewatch (www.scalewatch.com). All rights reserved.
+ * Please see accompanying LICENSE file for licensing terms.
+ */
 #pragma once
 
 #include <cstdint>
