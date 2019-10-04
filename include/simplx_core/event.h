e3923a6857ddca54aab6b6357092efcf5a3319d1
alineah <support@scalewatch.com>
2019-10-04 23:22:28 +0200

    Event type:the only structure allowed to be sent asynchronously (and efficiently) between actors.

---
+++ include/simplx_core/event.h
@@ -0,0 +1,17 @@
+/**
+ * @file event.h
+ * @brief event struct
+ * @copyright 2019 Scalewatch (www.scalewatch.com). All rights reserved.
+ * Please see accompanying LICENSE file for licensing terms.
+ */
+
+#pragma once
+
+namespace simplx
+{
+    
+    
+    
+} // namespace simplx
+
+    
