0e93e7b84d052ad939c836ee998eed7297f2d9b2
vvalerian <valerian.vives@gmail.com>
2019-10-05 00:25:52 +0200

    default base data exchanged by thread

--- include/pattern/bus/data.hpp
+++ include/pattern/bus/data.hpp
@@ -1,4 +1,9 @@
-
+/**
+ * @file data.hpp
+ * @brief default base data exchanged by thread
+ * @copyright 2019 Scalewatch (www.scalewatch.com). All rights reserved.
+ * Please see accompanying LICENSE file for licensing terms.
+ */
 #pragma once
 
 
