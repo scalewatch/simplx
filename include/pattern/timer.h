d6de710fdfbc51e8fda15293b7174d4efb13e2a3
loic <loic.barnault@tredzone.com>
2019-10-05 12:08:48 +0200

    groups all timer headers

---
+++ include/pattern/timer.h
@@ -0,0 +1,14 @@
+/**
+ * @file timer.h
+ * @brief Simplx timer
+ * @copyright 2019 Scalewatch (www.scalewatch.com). All rights reserved.
+ * Please see accompanying LICENSE file for licensing terms.
+ */
+ 
+ #pragma once
+ 
+ // timer
+ #include "pattern/timer/timeractor.h"
+ #include "pattern/timer/timerproxy.h"
+ #include "pattern/timer/timerevent.h"
+ 
